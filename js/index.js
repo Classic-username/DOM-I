463e3048aeff1e880b450fa950b02618f90467f8
Classic-username <josh.drapergame@gmail.com>
2019-06-05 22:09:25 -0600

    complete except changing color of last two anchors I added to the anchor list

--- js/index.js
+++ js/index.js
@@ -38,5 +38,70 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+// let logo = document.getElementById("logo-img");
+// logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+let nav = document.querySelector('nav')
+let navA = document.querySelectorAll('nav a')
+let firstImg = document.querySelector('#logo-img')
+let h1 = document.querySelector('h1')
+let button1 = document.querySelector('button')
+let secondImg = document.querySelector('#cta-img')
+let h4Main = document.querySelectorAll('.main-content h4')
+let pMain = document.querySelectorAll('.main-content p')
+let thirdImg = document.querySelector('#middle-img')
+let contactP = document.querySelectorAll('.contact p')
+let contactH = document.querySelector('.contact h4')
+let footerP = document.querySelector('footer p')
+console.log(contactP)
+
+
+navA[0].textContent = siteContent.nav['nav-item-1']
+navA[1].textContent = siteContent.nav['nav-item-2']
+navA[2].textContent = siteContent.nav['nav-item-3']
+navA[3].textContent = siteContent.nav['nav-item-4']
+navA[4].textContent = siteContent.nav['nav-item-5']
+navA[5].textContent = siteContent.nav['nav-item-6']
+
+firstImg.src = siteContent.nav['img-src']
+
+h1.textContent = siteContent.cta['h1']
+button1.textContent = siteContent.cta['button']
+secondImg.src = siteContent.cta['img-src']
+
+h4Main[0].textContent = siteContent["main-content"]['features-h4']
+h4Main[1].textContent = siteContent["main-content"]['about-h4']
+h4Main[2].textContent = siteContent["main-content"]['services-h4']
+h4Main[3].textContent = siteContent["main-content"]['product-h4']
+h4Main[4].textContent = siteContent["main-content"]['vision-h4']
+thirdImg.src = 'img/mid-page-accent.jpg'
+pMain[0].textContent = siteContent["main-content"]['features-content']
+pMain[1].textContent = siteContent["main-content"]['about-content']
+pMain[2].textContent = siteContent["main-content"]['services-content']
+pMain[3].textContent = siteContent["main-content"]['product-content']
+pMain[4].textContent = siteContent["main-content"]['vision-content']
+
+
+contactH.textContent = siteContent.contact['contact-h4']
+contactP[0].textContent = siteContent.contact['address']
+contactP[1].textContent = siteContent.contact['phone']
+contactP[2].textContent = siteContent.contact['email']
+
+footerP.textContent = siteContent.footer['copyright']
+
+
+
+let yo = document.createElement('a')
+let yoyo = document.createElement('a')
+
+yo.textContent = 'Yo'
+yoyo.textContent = 'Yoyo'
+
+
+nav.appendChild(yoyo)
+nav.prepend(yo)
+
+navA.forEach((object) => {
+  return object.style.color = 'green'
+})
